2a9b016bf3dae7a1c52cea920d8a9dd596e00304
Patrick Miller <apickledwalrus@icloud.com>
2025-12-15 11:20:46 -0500

    Add Component joining utility method

--- src/main/java/org/skriptlang/skript/bukkit/text/TextComponentUtils.java
+++ src/main/java/org/skriptlang/skript/bukkit/text/TextComponentUtils.java
@@ -5,6 +5,8 @@ import ch.njol.skript.lang.util.ConvertedExpression;
 import ch.njol.skript.registrations.Classes;
 import ch.njol.skript.util.LiteralUtils;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.ComponentLike;
+import net.kyori.adventure.text.JoinConfiguration;
 import org.jetbrains.annotations.Nullable;
 import org.skriptlang.skript.lang.converter.ConverterInfo;
 
@@ -16,6 +18,10 @@ public final class TextComponentUtils {
 	private static final ConverterInfo<Object, Component> OBJECT_COMPONENT_CONVERTER =
 		new ConverterInfo<>(Object.class, Component.class, TextComponentUtils::plain, 0);
 
+	private static final JoinConfiguration NEW_LINE_CONFIGURATION = JoinConfiguration.builder()
+		.separator(Component.newline())
+		.build();
+
 	/**
 	 * Creates a plain text component from an object.
 	 * @param message The message to create a component from.
@@ -23,6 +29,16 @@ public final class TextComponentUtils {
 	 */
 	public static Component plain(Object message) {
 		return Component.text(message instanceof String ? (String) message : Classes.toString(message));
+	}
+
+	/**
+	 * Joins components together with new line components.
+	 * @param components The components to join.
+	 * @return A component representing the provided components joined by new line components.
+	 * @see Component#newline()
+	 */
+	public static Component joinByNewLine(ComponentLike... components) {
+		return Component.join(NEW_LINE_CONFIGURATION, components);
 	}
 
 	/**
@@ -46,6 +62,6 @@ public final class TextComponentUtils {
 		return new ConvertedExpression<>(expression, Component.class, OBJECT_COMPONENT_CONVERTER);
 	}
 
-	private TextComponentUtils() { };
+	private TextComponentUtils() { }
 
 }
