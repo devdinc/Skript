51f53ebefcdddf51b117965f04761dac17521fad
Efnilite <35348263+Efnilite@users.noreply.github.com>
2026-01-05 14:53:09 +0100

    Move to common module, use FunctionReferenceParser

--- src/main/java/org/skriptlang/skript/common/CommonModule.java
+++ src/main/java/org/skriptlang/skript/common/CommonModule.java
@@ -11,7 +11,8 @@ public class CommonModule implements AddonModule {
 	@Override
 	public void load(SkriptAddon addon) {
 		try {
-			Skript.getAddonInstance().loadClasses("org.skriptlang.skript.common", "expressions");
+			Skript.getAddonInstance().loadClasses("org.skriptlang.skript.common",
+					"expressions", "sections");
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
